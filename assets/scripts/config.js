5bfa8533003d356139d7c990780fcdbb955471db
jshin02 <m.jinhyun.shin@gmail.com>
2020-07-06 16:46:05 -0400

    Fixed url in config.js file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://damp-shore-57998.herokuapp.com/',
+  production: 'https://damp-shore-57998.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
